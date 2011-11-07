33cadcf9d9aa50cbbce258df84dfd56d8d8ff58f
AJ ONeal <coolaj86@gmail.com>
2011-11-07 10:12:45 -0700

    handle input file change and made better a few css quirks

--- public/app.js
+++ public/app.js
@@ -15,20 +15,61 @@
   function handleDrop(ev) {
     console.log('I think you might have dropped something...')
     
-    //ev.stopPropagation();
     ev.preventDefault();
-    var files = ev.dataTransfer.files
+    // handles both drop and file input change events
+    var files = this.files || ev.dataTransfer.files
       , i
+      , file
+      , meta = []
       ;
 
     if (!files) {
-      console.log('... looks like litter to me.');
+      console.log('... looks drag-n-drop like litter to me.');
       return;
     }
 
-    for (i = 0; i < files.length; i += 1) {
-      console.log(files[i]);
+    if (!files.length) {
+      alert('looks like you tried to drop a folder, but your browser doesn\'t support that yet');
+      return;
     }
+    console.log();
+
+    for (i = 0; i < files.length; i += 1) {
+      file = files[i];
+      console.log(files[i]);
+      meta.push({
+          "type": file.type
+        , "name": file.name || file.fileName
+        , "size": file.size || file.fileSize
+        , "lastModifiedDate": file.lastModifiedDate
+        , "path": file.mozFullPath || file.webkitRelativePath
+      });
+      //file.xyz = 'something';
+      // XXX append to dom
+      $('#uploadlist').append('<li>' + file.name + '</li>');
+    }
+
+    console.log(JSON.stringify(files));
+
+    /*
+    request.post('/files/new', {}, meta).when(function (err, ahr, data) {
+      var formData = new FormData()
+        ;
+
+      formData.append('secret', data.secret)
+      data.tokens.forEach(function (token, j) {
+        formData.append(token, files[j]);
+        $($('#uploadlist li')[j]).append('<a href="/file/' + token + '">http://dropsha.re/file/' + token + '/' + file.name + '</a>');
+      });
+
+      // "global" upload queue
+      sequence.then(function () {
+        request.post('/files', {}, formData).when(function (err, ahr, data) {
+          $($('#uploadlist li')[j]).append('<span>Uploaded</span>');
+        });
+      });
+    });
+    */
   }
 
   function onMouseMove(ev) {
@@ -44,13 +85,14 @@
         || (ev.pageX < parentPos.left)
        ) {
       //$('input').css({ top: (parentPos.top + parentPos.bottom) / 2, left: parentPos.left + 15 });
-      $('input').css({ top: 10, left: 10 });
+      // TODO put far out -1000, -1000
+      chooser.css({ top: 10, left: 10 });
     }
     //console.log(ev.pageX, ev.pageY);
   }
 
   function onMouseLeave(ev) {
-    $('input').css({ top: -1000, left: -1000 });
+    chooser.css({ top: -1000, left: -1000 });
   }
 
   var parentPos 
@@ -60,12 +102,12 @@
   function addHandlers() {
     console.log('adding handlers');
     
-    $('input').css({'position': 'absolute'});
-
     parentPos = $('#dropzone').offset();
-    chooser = $('input[type=file]');
+    chooser = $('input#filechooser[type=file]');
     parentPos.right = parentPos.left + parentPos.width;
     parentPos.bottom = parentPos.top + parentPos.height;
+
+    $('body').delegate('#filechooser', 'change', handleDrop);
 
     $('body').delegate('#dropzone', 'dragover', handleDrag);
     $('body').delegate('#dropzone', 'drop', handleDrop);
