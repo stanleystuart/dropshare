fbdac9635e6832406ad01a2998607fa41696890d
Jamison Dance <jergason@gmail.com>
2011-12-02 17:56:37 -0700

    pass options from config.js to the dropshare instance

--- server.js
+++ server.js
@@ -2,13 +2,22 @@
   "use strict";
 
   var dropshare = require('./lib/index')
+    , config = require('./config')
     , options = {
           "tmp": "/tmp"
         , "files": __dirname + "/files"
         , "client": __dirname + "/public"
       }
-    , server = dropshare.create(options)
+    , server
+    , attributeName
     ;
 
+
+  // Use the options provided in the config.js file
+  for (attributeName in config) {
+    options[attributeName] = config[attributeName];
+  }
+
+  server = dropshare.create(options)
   module.exports = server;
 }());
